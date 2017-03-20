33e5940b53186f8e01b57bf821cdf605ed2e3d7f
gorzat <gorzat@gmail.com>
2017-03-20 10:17:24 +0100

    camelCase

--- 04 - CSS JS parser/colorStyles.js
+++ 04 - CSS JS parser/colorStyles.js
@@ -1,20 +1,22 @@
 const styles = [{ "class": "red", "params": [{ "name": "color", "value": "red" }] }, { "class": "green", "params": [{ "name": "background-color", "value": "green" }] }, { "class": "blue", "params": [{ "name": "position", "value": "absolute" }, { "name": "right", "value": "0" }, { "name": "top", "value": "10px" }] }];
 const root = document.body;
 
-function addStyle(styleCss, el, colorEl) {
-  for (k = 0; k < styleCss.length; k++) {
-    if (styleCss[k].class === colorEl) {
-      for (l = 0; l < styleCss[k].params.length; l++) {
-        let nameCss = styleCss[k].params[l].name;
-        if (nameCss === "background-color") {
-          nameCss = "backgroundColor";
-        }
-        let valueCss = styleCss[k].params[l].value;
-        el.style[nameCss] = valueCss;
+function addStyle(styleCss, el) {
+  for (l = 0; l < styleCss.length; l++) {
+    let nameCss = styleCss[l].name;
+    for (let i = 0; i < nameCss.length; i++) {
+      if (nameCss[i] === "-") {
+        let start = nameCss.substring(0, i);
+        let end = nameCss.substring(i + 2);
+        let up = nameCss[i + 1].toUpperCase();
+        nameCss = start + up + end;
       }
     }
+    let valueCss = styleCss[l].value;
+    el.style[nameCss] = valueCss;
   }
 }
+
 
 function testDepth(el, depth) {
   for (var i = 0; el.children.length > i; i++) {
@@ -22,7 +24,7 @@ function testDepth(el, depth) {
     for (var j = 0; j < styles.length; j++) {
       var colorEl = styles[j].class;
       if (el.children[i].classList.contains(colorEl)) {
-        addStyle(styles, el.children[i], colorEl);
+        addStyle(styles[j].params, el.children[i]);
       }
     }
   }

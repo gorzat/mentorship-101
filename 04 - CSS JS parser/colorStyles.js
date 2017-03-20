4a63758eaccaa0aa1235659aad1912703bc1de14
gorzat <gorzat@gmail.com>
2017-03-20 12:35:30 +0100

    changes

--- 04 - CSS JS parser/colorStyles.js
+++ 04 - CSS JS parser/colorStyles.js
@@ -1,24 +1,31 @@
 const styles = [{ "class": "red", "params": [{ "name": "color", "value": "red" }] }, { "class": "green", "params": [{ "name": "background-color", "value": "green" }] }, { "class": "blue", "params": [{ "name": "position", "value": "absolute" }, { "name": "right", "value": "0" }, { "name": "top", "value": "10px" }] }];
 const root = document.body;
 
-function toCamelCase(nameCss) {
-  for (let i = 0; i < nameCss.length; i++) {
-    if (nameCss[i] === "-") {
-      let start = nameCss.substring(0, i);
-      let end = nameCss.substring(i + 2);
-      let up = nameCss[i + 1].toUpperCase();
-      nameCss = start + up + end;
+function toCamelCase(nameTest) {
+  for (let i = 0; i < nameTest.length; i++) {
+    if (nameTest[i] === "-") {
+      const start = nameTest.substring(0, i);
+      const end = nameTest.substring(i + 2);
+      const up = nameTest[i + 1].toUpperCase();
+      let camelName = start + up + end;
+      return camelName;
     }
   }
 }
 
 function addStyle(styleCss, el) {
   for (l = 0; l < styleCss.length; l++) {
-    toCamelCase(styleCss[l].name);
+    let nameCss = styleCss[l].name;
+    let nameCssCamel = toCamelCase(styleCss[l].name);
     let valueCss = styleCss[l].value;
-    el.style[styleCss[l].name] = valueCss;
+    if (nameCssCamel != null) {
+      el.style[nameCssCamel] = valueCss;
+    } else {
+      el.style[nameCss] = valueCss;
+    }
   }
 }
+
 
 function testDepth(el, depth) {
   for (var i = 0; el.children.length > i; i++) {

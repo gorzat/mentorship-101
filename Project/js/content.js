2ba659b4c27941079f4a10ced4e77146d91871c8
gorzat <gorzat@gmail.com>
2017-03-28 10:09:12 +0200

    changes, cleaning up

--- Project/js/content.js
+++ Project/js/content.js
@@ -1,94 +1,79 @@
-const b = new URL('https://api.citybik.es/v2/networks');
-const w = new URL("https://simple.wikipedia.org/w/api.php?action=opensearch&origin=*");
-//let searchFor = [];
+const bikeAPI = new URL('https://api.citybik.es/v2/networks');
+const wikipediaAPI = new URL("https://simple.wikipedia.org/w/api.php?action=opensearch&origin=*");
+const locations = [];
 
-let locations = new Array;
+bikeAPI.searchParams;
 
-
-b.searchParams;
-
-fetch(b)
+fetch(bikeAPI)
     .then(response => response.json())
-    //.then(data => console.log(data.networks[0].location.city))
     .then(data => displayCities(data.networks))
     .catch(error => console.error(error.message));
 
 function displayCities(data) {
-    //console.log(locations);
+    const list = document.getElementsByClassName('cities-list')[0];
     for (let i = 0; i < 10; i++) {
-        const list = document.getElementsByClassName('cities-list')[0];
-        const el = document.createElement('li');
-        const ela = document.createElement('article');
-        const elhe = document.createElement('header');
-        const elh = document.createElement('h3');
+        const elLi = document.createElement('li');
+        const elArticle = document.createElement('article');
+        const elHeader = document.createElement('header');
+        const elH = document.createElement('h3');
 
-        el.classList.add('cities-list__element');
-        ela.classList.add('city-box')
-        elhe.classList.add('city-box__top');
-        elh.classList.add('city-box__title');
+        elLi.classList.add('cities-list__element');
+        elArticle.classList.add('city-box')
+        elHeader.classList.add('city-box__top');
+        elH.classList.add('city-box__title');
 
-        const lel = list.appendChild(el);
-        const listli = list.getElementsByTagName('li')[i];
-        listli.appendChild(ela);
-        ela.appendChild(elhe);
-        elhe.appendChild(elh);
+        const listEl = list.appendChild(elLi);
+        const listCurrentLi= list.getElementsByTagName('li')[i];
+        listCurrentLi.appendChild(elArticle);
+        elArticle.appendChild(elHeader);
+        elHeader.appendChild(elH);
         let text = data[i].location.city;
         let cityName = document.createTextNode(text);
-        elh.appendChild(cityName);
+        elH.appendChild(cityName);
 
-        //elp.classList.add('test2');
-        //searchFor.push(text);
-        getArticleList(text, listli);
+        getArticleList(text, listEl);
 
         let currentLocation = new Object();
-        currentLocation.lat = Math.round(data[i].location.latitude * 1000)/1000;
-        //console.log(currentLocation.lat);
-        currentLocation.lng = Math.round(data[i].location.longitude*1000)/1000;
-
-
+        currentLocation.lat = Math.round(data[i].location.latitude * 1000) / 1000;
+        currentLocation.lng = Math.round(data[i].location.longitude * 1000) / 1000;
         locations.push(currentLocation);
     }
-    //console.log(locations);
     initMap(locations);
 };
 
-//console.log(locations[0]);
 
 function initMap() {
-
     var start = locations[0];
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
         center: start
     });
 
-const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    for(j=0; j<locations.length; j++) {
-        // console.log(locations[j]);
+    for (j = 0; j < locations.length; j++) {
         new google.maps.Marker({
-        position: locations[j],
-        label: labels[j],
-        map: map
-    });
+            position: locations[j],
+            label: labels[j],
+            map: map
+        });
     }
 }
 
+function getArticleList(searchFor, listEl) {
+    wikipediaAPI.searchParams.append("search", searchFor);
 
-function getArticleList(searchFor2, listli) {
-    w.searchParams.append("search", searchFor2);
-
-    fetch(w)
+    fetch(wikipediaAPI)
         .then(response => response.json())
-        .then(data2 => {
-            const articleEl = listli.firstChild;
-            const elp = document.createElement('p');
-            let newT = data2[2];
-            let cityDesc = document.createTextNode(newT[0]);
+        .then(dataWiki => {
+            const articleEl = listEl.firstChild;
+            const elP = document.createElement('p');
+            let wikiText = dataWiki[2];
+            let cityDesc = document.createTextNode(wikiText[0]);
 
-            articleEl.appendChild(elp);
-            elp.appendChild(cityDesc);
-            elp.classList.add('city-box__description');
+            articleEl.appendChild(elP);
+            elP.appendChild(cityDesc);
+            elP.classList.add('city-box__description');
         })
         .catch(error => console.error(error.message));
 };
